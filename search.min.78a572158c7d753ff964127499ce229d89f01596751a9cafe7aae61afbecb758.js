75f23769b67081596d5ee414484236cd61caec0f
GitHub Action <action@github.com>
2025-04-22 00:32:50 +0000

    Commit by Github Action at DATE: 2025-04-22 TIME: 00:32:49

--- search.min.78a572158c7d753ff964127499ce229d89f01596751a9cafe7aae61afbecb758.js
+++ search.min.78a572158c7d753ff964127499ce229d89f01596751a9cafe7aae61afbecb758.js
@@ -1,4 +1,4 @@
-(()=>{(function(){const s=new URLSearchParams(window.location.search),t=s.get("search").trim(),o=document.getElementById("search-input"),e=document.getElementById("search-result"),n=document.getElementById("search-result-empty");if(t)o.value=t,a(t);else return;const i={includeScore:!0,includeMatches:!0,minMatchCharLength:searchOptions.minMatchCharLength,threshold:searchOptions.threshold,keys:[{name:"title",weight:.8},{name:"content",weight:.5},{name:"tags",weight:.2},{name:"categories",weight:.2}]};function a(t){fetch("./index.json").then(e=>e.json()).then(s=>{const a=new Fuse(s,i),o=a.search(t);o.length>0?(c(t,o),n.classList.add("hidden")):(e.innerHTML="",n.classList.remove("hidden"))})}const r=`
+(()=>{(function(){const s=new URLSearchParams(window.location.search),t=s.get("search").trim(),o=document.getElementById("search-input"),e=document.getElementById("search-result"),n=document.getElementById("search-result-empty");if(t)o.value=t,a(t);else return;const i={ignoreLocation:!0,includeScore:!0,includeMatches:!0,minMatchCharLength:searchOptions.minMatchCharLength,threshold:searchOptions.threshold,keys:[{name:"title",weight:.8},{name:"content",weight:.5},{name:"tags",weight:.2},{name:"categories",weight:.2}]};function a(t){fetch("./index.json").then(e=>e.json()).then(s=>{const a=new Fuse(s,i),o=a.search(t);o.length>0?(c(t,o),n.classList.add("hidden")):(e.innerHTML="",n.classList.remove("hidden"))})}const r=`
   <article class="flex flex-col gap-y-3 p-6 mt-6 mx-2 md:mx-0 rounded-lg shadow-md bg-white dark:bg-gray-700" id="result-{{= it.index }}">
     <h2 class="text-4xl font-semibold text-slate-800 dark:text-slate-200">
       <a href="{{= it.permalink }}">{{! it.title }}</a>
@@ -9,19 +9,19 @@
     <ul class="flex flex-row flex-wrap text-slate-500 dark:text-slate-300">
       {{~ it.categories :v }}
       <li>
-        <span
+        <a href="{{! v.RelPermalink }}"
           class="text-sm mr-2 px-2 py-1 rounded border border-emerald-800 bg-emerald-800 text-slate-50">
-          {{!v}}
-        </span>
+          {{! v.LinkTitle }}
+        </a>
       </li>
       {{~}}
       {{~ it.tags :v }}
       <li>
-        <span
+        <a href="{{! v.RelPermalink }}"
           class="flex flex-row text-sm mr-2 py-1">
           {{= it.tagIcon }}
-          <span class="ml-0">{{!v}}</span>
-        </span>
+          <span class="ml-0">{{! v.LinkTitle }}</span>
+        </a>
       </li>
       {{~}}
     </ul>
